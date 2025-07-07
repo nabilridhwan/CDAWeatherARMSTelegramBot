26d8fe99c198f59b359a8f0688894421089dced4
Nabil Ridhwan <nabridhwan@gmail.com>
2025-07-07 12:07:17 +0800

    feat: implement winston logger for improved logging functionality

---
+++ utils/logger.ts
@@ -0,0 +1,20 @@
+import winston from "winston";
+const { combine, timestamp, json, errors } = winston.format;
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: combine(errors({ stack: true }), timestamp(), json()),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }),
+        new winston.transports.File({
+            filename: 'logs/app.log',
+        }),
+    ],
+});
+
+export default logger
