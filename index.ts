0c34321990a2683fe255003fb192d0b1a607d860
Nabil Ridhwan <nabridhwan@gmail.com>
2025-07-08 23:37:31 +0800

    chore: implement secret token generation and validation for webhook

--- index.ts
+++ index.ts
@@ -5,12 +5,25 @@ import {readFile} from 'node:fs/promises'
 import express,{ type Request, type Response } from 'express';
 import {bot, job} from "./utils/bot";
 import helmet from 'helmet'
+import "./utils/generateSecretToken";
 
 const app = express()
-bot.telegram.setWebhook(`${process.env.HOST || 'http://localhost:8080'}/telegram-webhook`)
+
+bot.telegram.setWebhook(`${process.env.HOST || 'http://localhost:8080'}/telegram-webhook`, {
+    secret_token : process.env.SECRET_TOKEN,
+})
 
 app.use(helmet());
 app.use(bot.webhookCallback('/telegram-webhook'))
+
+app.use('/telegram-webhook', (req: any, res:any, next) => {
+    const token = req.get("X-Telegram-Bot-Api-Secret-Token");
+    if (token !== process.env.SECRET_TOKEN){
+        logger.error("Unauthorized access attempt detected from IP:", req.ip);
+        return res.sendStatus(403);
+    }
+    next();
+});
 
 app.get("/logs", async (req: Request, res: Response) => {
     try {
---
+++ utils/generateSecretToken.ts
@@ -0,0 +1,4 @@
+import crypto from 'crypto'
+
+// Inject the secret token into the environment variable
+process.env.SECRET_TOKEN = crypto.randomBytes(32).toString('base64url')
